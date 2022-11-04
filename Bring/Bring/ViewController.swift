3f96a4b128825450e2330a455a4c09fffc66cefc
yexjin <tnska0421@naver.com>
2022-11-04 15:33:28 +0900

    [INIT] 프로젝트 세팅

---
+++ Bring/Bring/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bring
+//
+//  Created by 오예진 on 2022/11/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
