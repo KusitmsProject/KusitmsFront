1058a701a6e03f71195fdd535692db4144b2a1b7
yexjin <tnska0421@naver.com>
2022-11-17 00:35:03 +0900

    [FEAT] 지도화면 Apple Map 사용

---
+++ Bring/Bring/PrevMe/Map/MapView.swift
@@ -0,0 +1,22 @@
+//
+//  MapView.swift
+//  Bring
+//
+//  Created by 오예진 on 2022/11/16.
+//
+
+import UIKit
+
+class MapView: UIView {
+
+    @IBOutlet weak var sdfsdf: UIButton!
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
