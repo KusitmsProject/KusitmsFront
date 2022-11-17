0927eb201122e496f89f826bf4110d39eb1b874b
yexjin <tnska0421@naver.com>
2022-11-17 23:42:56 +0900

    [FEAT] alamofire 설치

--- Bring/Bring/Models/PrevmeRecord.swift
+++ Bring/Bring/Models/PrevmeRecord.swift
@@ -7,11 +7,24 @@
 
 import Foundation
 
-struct PrevmeData: Hashable {
-    let _id: String
-    var music: String
+// MARK: - Welcome
+struct PrevmeData: Codable {
+    // userIdx 필수
+    // date, options 필수
+    // options : 그때의 나 = 0, 오늘의 나 = 1
+    let userIdx: Int
+    let date, emotion, season: String
+    let weather: [String]
+    let lyrics, placeNickname, place: String
+    let imageURL: String
+    let record, track, artist: String
+    let friendList: [String]
+    let options: Int
+
+    enum CodingKeys: String, CodingKey {
+        case userIdx, date, emotion, season, weather, lyrics, placeNickname, place
+        case imageURL = "imageUrl"
+        case record, track, artist, friendList, options
+    }
 }
 
-extension PrevmeData {
-    static var data = PrevmeData(_id: " ", music: "")
-}
